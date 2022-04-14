7a55a7d0744a707234014ab69a538eec8d1fbd83
Swati Goel <sgoel@college.harvard.edu>
2022-04-14 15:53:42 -0400

    generated random files!!

---
+++ gen_files.py
@@ -0,0 +1,11 @@
+import random
+
+n = 50
+m = 100
+r = 10**12 #confirmed random.random() w sufficient precision, docs: https://docs.python.org/3/library/random.html
+
+for pre in range(1, n+1):
+    with open("test"+str(pre)+".txt", 'w') as f:
+        for x in range(0, m):
+            f.write(str(random.randint(1,10**12)))
+            f.write('\n')
